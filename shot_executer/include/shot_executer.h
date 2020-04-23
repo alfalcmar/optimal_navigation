c14e70388eba154b1c8d968fc02419c430894e40
alfalcmar <a.alcantara.marin@gmail.com>
2020-04-23 17:27:18 +0200

    shot executer minor changes and documentation

--- shot_executer/include/shot_executer.h
+++ shot_executer/include/shot_executer.h
@@ -20,28 +20,33 @@
 #include <std_msgs/Float32.h>
 
 
-/* A name class 
 /**
-*  The basic class description, it does:
-*  TODO: target velocity
-*  TODO
+*  \brief The basic class description, it does:
 */
 class ShotExecuter
 {    
     public:
+        /** \brief Generic constructor of the class. This constructor reads generic parameters and subscribes to topics.
+         *  \param _nh public nodehandle
+         */
         ShotExecuter(ros::NodeHandle &_nh);
     protected:
-        ros::NodeHandle nh;
-        std::string target_topic_;
-        ros::ServiceServer shooting_action_srv_;
-        ros::Publisher desired_pose_pub_;
-        ros::Publisher target_trajectory_pub_;
-        ros::Subscriber target_pose_sub_;
-        bool takeoff_called_succesfully_ = false;
+
+        //ROS (publishers, subscribers)
+        ros::NodeHandle nh;   /*< Nodehandle */
+        std::string target_topic_;  /*< Target topic */
+        ros::ServiceServer shooting_action_srv_; /*< Server to choose the action */
+        ros::Publisher desired_pose_pub_;        /*< That publish the desired pose */
+        ros::Publisher target_trajectory_pub_;   /*< That publish the target trajectory prediction */
+        ros::Subscriber target_pose_sub_;       /*< Subscribe to target pose */
+        bool takeoff_called_succesfully_ = false;   /*< Flag for take off */
+
         // inputs
-        nav_msgs::Odometry target_pose_;
-        nav_msgs::Odometry drone_pose_;
-         //shooting action fields
+        nav_msgs::Odometry target_pose_;            /*< target pose */
+        nav_msgs::Odometry drone_pose_;             /*< drone pose*/
+
+        /* Struct to define the shooting action
+        */
         struct shooting_action{
             std::string start_event = "";
             int shooting_action_type = 0;
@@ -50,29 +55,61 @@ class ShotExecuter
             int target_type;
             geometry_msgs::Vector3 rt_parameters; 
         };
-        // states
-        Eigen::Vector3f camera_angles_;
+        Eigen::Vector3f camera_angles_; /**< member to save the angles of the camera to point the target. It is calculate by calculateGimbalAngles*/
         const int yaw = 1;
         const int pitch = 2;
 
         // parameters
         int drone_id_ = 1; // TODO initialize by constructor
-        int step_size_;
-        const int time_horizon_= 10; //TODO read as parameter
-        float rate_pose_publisher_ = 5; //Hz
-        float rate_camera_publisher_ = 10; //Hz
-        std::thread action_thread_;
-        std::thread camera_thread_;
+        int step_size_;    /**< step size (seconds) */
+        const int time_horizon_= 10; /*< Number of steps */ 
+        float rate_pose_publisher_ = 5; /**< Rate to publish the desired pose (Hz) */
+        float rate_camera_publisher_ = 10; /**< Rate to publish the camera pose (Hz) */
+        std::thread action_thread_;  /*< thread that publish the desired pose */
+        std::thread camera_thread_;  /*< thread that publish the pith and yaw needed for the camera pointing to the target */
+        
+        bool new_shooting_action_received_ = false;
+        bool shooting_action_running_ = false;
+        /** \brief This predicts the target trajectory over time_horizon_. We use a velocity cte model.
+         *          Besides, publish the predicted trajectory for visualization
+         *  \return The predicted target trajectory 
+         */
         std::vector<nav_msgs::Odometry> targetTrajectoryPrediction();
+
+        /** \brief Calculate desired pose. If type flyby, calculate wrt last mission pose . If lateral, calculate wrt time horizon pose
+         *  \TODO   z position and velocity, angle relative to target
+         *  \TODO   calculate orientation by velocity and apply it to desired pose
+         */
         nav_msgs::Odometry calculateDesiredPoint(const struct shooting_action _shooting_action, const std::vector<nav_msgs::Odometry> &target_trajectory);
+        /** \brief Callback for action service. Receive the request and start a thread with this request actionThread()
+         *         If there are any shooting action active, it command it to finish and wait it to finish
+         */
         bool actionCallback(shot_executer::ShootingAction::Request  &req, shot_executer::ShootingAction::Response &res);
+        /** \brief This function predicts the target trajectory and calculates the desired pose while the shooting action lasts or while a new one is received.
+         *  \param _shooting_action shooting action to execute 
+         *  \TODO implement time_reached
+         *  \TODO implement distance_reached
+         */
         void actionThread(struct shooting_action _shooting_action);
+        /** \brief Callback for the target pose
+         */
         void targetPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& _msg);
+        /** \brief This function calculates the camera angles needed to point to the target.
+         *         It is saved in camera_angles_ member
+         */
         void calculateGimbalAngles();
+        /** This thread is publishing camera_angles_ every time.
+         */
         void cameraThread();
+        /** \brief prototype function to call the camera topic
+         */
         virtual void publishCameraCommand();
 };
 
+/**
+ * \TODO: target velocity is set as 0
+
+ */
 class ShotExecuterMRS : public ShotExecuter{
     public:
         ShotExecuterMRS(ros::NodeHandle &_nh);
@@ -98,6 +135,10 @@ class ShotExecuterUAL : public ShotExecuter{
         ros::ServiceClient take_off_srv_;
         ros::ServiceClient go_to_waypoint_client_;
         ros::ServiceClient land_client_;
+        /** \brief Taking off the drone
+        *  \param _height   take off's height
+        *  \return success
+        **/
         bool takeOff(const double _height);
 
 
