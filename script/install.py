784df6dcbb8f2857a790d39f64017e1ca87f8150
alfalcmar <a.alcantara.marin@gmail.com>
2020-07-23 12:06:44 +0200

    removing previous acado installation

--- script/install.py
+++ script/install.py
@@ -10,6 +10,7 @@ print(path)
 # 	os.environ["FORCES"]= "true"
 # a = input("Do you want to install the optimal navigation toolkit with ACADO toolkit solver? Press y/n")
 # if(a=="y"):
+
 os.environ["ACADO"] = "true"
 os.system('git submodule init')
 os.system('git submodule update')
@@ -21,8 +22,12 @@ if(not os.path.isdir('build')):
 os.chdir('build')
 os.system('cmake ..')
 os.system('make')
-os.system('sudo make install')
 
+if(os.path.isdir('/usr/local/share/acado')):
+	cmd = 'sudo rm -r /usr/local/share/acado'
+	
+
+os.system('sudo make install')
 
 cmd = 'catkin build'
 os.system(cmd)
